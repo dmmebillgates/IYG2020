e7d48cdbfcef31723d77a08c1e6ac0c9ce23f628
dmmebillgates <jan766078@gmail.com>
2019-11-16 16:48:29 +0100

    player movement update

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -5,27 +5,20 @@ using UnityEngine;
 public class PlayerMovement2D : MonoBehaviour{
     public CharacterController2D controller;
     public float runSpeed = 40f;
-    float horizontalMove = 0f;
+    float horizontalMove = 10f;
     bool jump = false;
-    bool crouch = false;
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
-        }else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = true;
-        }else if (Input.GetButtonUp("Crouch"))
-        {
-            crouch = false;
         }
     }
 
     void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
         jump = false;
     }
 }
