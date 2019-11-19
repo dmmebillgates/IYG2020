d79a7028e98b201067ee9a8b5981d23929c83ad2
dmmebillgates <jan766078@gmail.com>
2019-11-19 19:53:11 +0100

    New animations
    Added jumping, running and idle

--- Assets/Scripts/PlayerMovement2D.cs
+++ Assets/Scripts/PlayerMovement2D.cs
@@ -5,6 +5,7 @@ public class PlayerMovement2D : MonoBehaviour
     // Set in unity
     public CharacterController2D controller;
     public float runSpeed = 100f;
+    public Animator animator;
     
     // Default values
     float horizontalMove = 10f;
@@ -14,9 +15,12 @@ public class PlayerMovement2D : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButton("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         } else if (Input.GetButtonDown("Crouch"))
         {
             crouch = true;
@@ -25,6 +29,10 @@ public class PlayerMovement2D : MonoBehaviour
         {
             crouch = false;
         }
+    }
+    public void Landing()
+    {
+        animator.SetBool("IsJumping", false);
     }
 
     void FixedUpdate()
