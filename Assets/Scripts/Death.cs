eb9e933135db06303a386d62c0f36780e4d1beca
CowMan <pawelniewiarowski2001@gmail.com>
2019-12-07 16:43:48 +0100

    Added death system

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour
+{
+    // Player and objects what can kill him
+    public Collider2D player;
+    public Collider2D deathMap;
+
+    void Update()
+    {
+        if(player.IsTouching(deathMap))
+        {
+            int sameScene = SceneManager.sceneCountInBuildSettings - 1;
+            SceneManager.LoadScene(sameScene);
+        }
+    }
+}
