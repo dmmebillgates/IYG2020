826c26b9566b9f6d344c289689d87503182638dc
CowMan <pawelniewiarowski2001@gmail.com>
2019-12-07 16:54:35 +0100

    Added simple menu

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    // Load level from Scene Build yeahhhhhhh boii
+    public int sceneCount;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(sceneCount);
+        }
+    }
+}
