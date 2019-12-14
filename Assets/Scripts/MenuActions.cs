d044cf07a89afb14825579f6503560eb92446610
CowMan <pawelniewiarowski2001@gmail.com>
2019-12-14 11:32:35 +0100

    Finshed main menu

---
+++ Assets/Scripts/MenuActions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    // Main menu script
+    public string action;
+    public int sceneCount;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Check action
+            switch (action)
+            {
+                // Load level
+                case "load": SceneManager.LoadScene(sceneCount); break;
+
+                // Exit
+                case "exit": Application.Quit(); break;
+            }
+        }
+    }
+}
