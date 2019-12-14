4a2bd8f6c34db22007c659fde8f3199ac44568e1
CowMan <pawelniewiarowski2001@gmail.com>
2019-12-14 15:30:37 +0100

    Small fixes

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void startGame()
+    {
+        // Load next level in queue
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void exitGame()
+    {
+        // Exit game
+        Application.Quit();
+    }
+
+}
