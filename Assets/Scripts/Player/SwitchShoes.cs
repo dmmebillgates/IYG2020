4598dd6e7754537d92c0ec7ee4abd8febd63eb80
CowMan <pawelniewiarowski2001@gmail.com>
2019-12-24 01:21:21 +0100

    Addes first shoe
    First shoe increase player speed

--- Assets/Scripts/Player/SwitchShoes.cs
+++ Assets/Scripts/Player/SwitchShoes.cs
@@ -1,28 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SwitchShoes : MonoBehaviour
 {
     public PlayerMovement2D PlayerMovement2D;
+    public CharacterController2D PlayerController2D;
+
     public int CurrentShoe;
+
+    // Default values
+    public int defaultSpeed = 25;
+    public int defaultJump;
+
+    // Increase values
+    public int increaseSpeed = 50;
+    public int increaseJump;
+
     void Update()
     {
         if (Input.GetButton("Shoe0"))
         {
+            // Default shoe
             CurrentShoe = 0;
+            PlayerMovement2D.runSpeed = defaultSpeed;
         }
-        if (Input.GetButton("Shoe1"))
+        else if (Input.GetButton("Shoe1"))
         {
+            // Increase your movment speed if you choose this shoe
             CurrentShoe = 1;
+            PlayerMovement2D.runSpeed = increaseSpeed;
+
         }
-        if (Input.GetButton("Shoe2"))
+        else if (Input.GetButton("Shoe2"))
         {
             CurrentShoe = 2;
+            PlayerMovement2D.runSpeed = defaultSpeed;
         }
-        if (Input.GetButton("Shoe3"))
+        else if (Input.GetButton("Shoe3"))
         {
             CurrentShoe = 3;
+            PlayerMovement2D.runSpeed = defaultSpeed;
         }
 
     }
