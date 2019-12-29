1fcc0a8eaa20b4c7a955ce9a33985f35dab7f9a1
CowMan <pawelniewiarowski2001@gmail.com>
2019-12-29 18:06:18 +0100

    Build First Level

--- Assets/Scripts/Player/PlayerMovement2D.cs
+++ Assets/Scripts/Player/PlayerMovement2D.cs
@@ -15,21 +15,18 @@ public class PlayerMovement2D : MonoBehaviour
     public float horizontalMove = 10f;
     public bool jump = false;
     public bool crouch = false;
-    public bool end = false;
+    public bool End = false;
+    private bool m_Run = false;
 
     void Update()
     {
-        if (playerPosition.position.x < stopPosition.position.x - spaceToStop)
-        {
-            horizontalMove = 1 * runSpeed;
-        }
-        else
-        {
-            horizontalMove = 0 * runSpeed;
-        }
-
+        // Check end position
+        if (playerPosition.position.x < stopPosition.position.x - spaceToStop && m_Run) horizontalMove = 1 * runSpeed;
+        else horizontalMove = 0 * runSpeed;
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
-
+        
+        // Input's
+        if (Input.GetKeyDown(KeyCode.D)) m_Run = true;
         if (Input.GetButton("Jump"))
         {
             jump = true;
