f7f4dcb874ada16cd021599cc08e6aacb25eff0a
dmmebillgates <jan766078@gmail.com>
2020-01-22 15:48:18 +0100

    Added background and made it into prefab

---
+++ Assets/follow_player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_player : MonoBehaviour
+{
+    public Transform Player;
+    private Transform background;
+    void Update()
+    {
+        background = Player;
+            
+
+        
+    }
+}
