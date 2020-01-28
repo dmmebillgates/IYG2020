ef4403207bdc69b1fff1864bc3c7c1a230451d37
CowMan <pawelniewiarowski2001@gmail.com>
2020-01-28 20:17:53 +0100

    Updated PauseMenu.cs

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -5,45 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-
-
     public static bool GameIsPaused = false;
     public GameObject PauseMenuUI;
+
     void Update()
     {
-        Cursor.visible = true;
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
-            {
-                Resume();
-            }
-            else
-            {
-                PauseGame();
-            }
+            if (GameIsPaused) Resume();
+            else PauseGame();
         }
         
     }
+    
     public void Resume()
     {
         PauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
-
+        Cursor.visible = false;
     }
+
     void PauseGame()
     {
         PauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
     }
-    public void LoadMenu()
-    {
-        SceneManager.LoadScene("Menu");
-    }
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+
+    public void LoadMenu() => SceneManager.LoadScene("Menu");
+
+    public void QuitGame() => Application.Quit();
 }
